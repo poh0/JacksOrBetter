ad2255dddf4bdaadd6db0ce3515fda1a2f515e56
Lauri Pöhö <lauri.poho@gmail.com>
2025-03-21 19:57:35 +0200

    inital commit

---
+++ src/JacksOrBetter.cpp
@@ -0,0 +1,6 @@
+#include "JacksOrBetter.hpp"
+
+JacksOrBetter::JacksOrBetter()
+{
+    
+}
---
+++ src/JacksOrBetter.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class JacksOrBetter {
+private:
+    sf::RenderWindow m_Window;
+
+public:
+    JacksOrBetter();
+    ~JacksOrBetter() {};
+};
